c86923ce2b1596dc6301c568297850768394b04e
aliyutmk <40960117+aliyutmk@users.noreply.github.com>
2023-11-14 20:13:10 +0100

    mesa agents

--- demo.py
+++ demo.py
@@ -2,17 +2,32 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 
-class customer(mesa.Agent):
+class Customer(mesa.Agent):
   """A customer who want to rent an EV for mobility need"""
   def __init__(self, unique_id, model):
     super().__init__(unique_id, model)
+    """ Agent's resource"""
     self.wealth = 1
+  def step(self):
+    print(f"Hi, I am an agent, you can call me {str(self.unique_id)}.")
 
 
-class customerModel(mesa.Model):
-  def __init__(self, N):
-    """Number of customer agents"""
-    self.num_agents = N
-    """create agents"""
-    for i in range(N):
-      a = customer(i, self)
+class CustomerModel(mesa.Model):
+    """A model with some number of agents."""
+
+    def __init__(self, N):
+        self.num_agents = N
+        # Create scheduler and assign it to the model
+        self.schedule = mesa.time.RandomActivation(self)
+
+        # Create agents
+        for i in range(self.num_agents):
+            a = Customer(i, self)
+            # Add the agent to the scheduler
+            self.schedule.add(a)
+
+    def step(self):
+        """Advance the model by one step."""
+
+        # The model's step will go here for now this will call the step method of each agent and print the agent's unique_id
+        self.schedule.step()
